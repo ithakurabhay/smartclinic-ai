bde360599723f72533aa9ac7182305c91eacd924
AnshSrivastava9795 <anshsrivastava72004@gmail.com>
2026-08-12 20:30:35 +0530

    Use Render port

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN;
